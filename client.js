1b57c8d946606fc2f4f7a1d98420186409997c73
DLMazk <daniel.patrick.litterst@hs-furtwangen.de>
2023-12-11 12:13:50 +0100

    authentication und lokale adresse des brokers

--- client.js
+++ client.js
@@ -6,11 +6,12 @@ const options = {
     clean: true,
     connectTimeout: 4000,
     // Authentication
-    clientId: 'web_client'
-    // username: 'emqx_test',
-    // password: 'emqx_test',
+    clientId: 'web_client',
+    username: 'DL_Client',
+    password: 'litterst17',
 }
-const url = "ws://95.208.10.86:9001";
+// const url = "ws://95.208.10.86:9001";
+const url = "ws://10.136.2.179:9001";
 // const url = "ws://localhost:9001";
 // const url = 'ws://broker.emqx.io:8083/mqtt'
 
