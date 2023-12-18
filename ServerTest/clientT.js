97e7263454554ad275a4ac36765f8c34a3a9311e
DLMazk <daniel.patrick.litterst@hs-furtwangen.de>
2023-12-18 14:26:38 +0100

    Send Button hinzugefügt

--- ServerTest/clientT.js
+++ ServerTest/clientT.js
@@ -1,32 +1,38 @@
 import { request } from 'http';
+import { send } from 'process';
 import { stringify } from 'querystring';
 
 const postData = stringify({
-  message: 'Hallo Server! Ich bin ein Client.'
+    message: 'Hallo Server! Ich bin ein Client.'
 });
 
 const options = {
-  hostname: '192.168.178.101', // Ersetze SERVER_IP_ADRESSE mit der IP-Adresse deines Servers
-  port: 3001, // Der Port, auf dem der Server läuft
-  path: '/message',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': Buffer.byteLength(postData)
-  }
+    hostname: '192.168.178.101', // Ersetze SERVER_IP_ADRESSE mit der IP-Adresse deines Servers
+    port: 3001, // Der Port, auf dem der Server läuft
+    path: '/message',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(postData)
+    }
 };
 
 const req = request(options, (res) => {
-  console.log(`Statuscode: ${res.statusCode}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`Body: ${chunk}`);
-  });
+    console.log(`Statuscode: ${res.statusCode}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+        console.log(`Body: ${chunk}`);
+    });
 });
 
-req.on('error', (e) => {
-  console.error(`Problem mit der Anfrage: ${e.message}`);
-});
+let sendBtn = document.getElementById("send");
+sendBtn.addEventListener("click", sending);
 
-req.write(postData);
-req.end();
+function sending() {
+    req.on('error', (e) => {
+        console.error(`Problem mit der Anfrage: ${e.message}`);
+    });
+
+    req.write(postData);
+    req.end();
+}
